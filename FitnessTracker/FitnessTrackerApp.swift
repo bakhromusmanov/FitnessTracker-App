d82e07e1d2538e04ff2216842e10f5cfd32da361
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2024-10-14 15:27:06 +0500

    Initial Commit

---
+++ FitnessTracker/FitnessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerApp.swift
+//  FitnessTracker
+//
+//  Created by Bakhrom Usmanov on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
